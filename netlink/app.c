b955b81963864a82f94dbaf638ed3e52748aa765
Wang Lu <cnwanglu@icloud.com>
2024-11-27 00:23:26 +0800

    init netlink, not finished

---
+++ netlink/app.c
@@ -0,0 +1,13 @@
+// user_program.c
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <linux/netlink.h>
+
+#define NETLINK_USER 31
+
+int main() {
+  int sock_fd;
+}
---
+++ netlink/kern.c
@@ -0,0 +1,6 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/netlink.h>
+#include <linux/socket.h>
+#include <linux/init.h>
+#include <linux/errno.h>
